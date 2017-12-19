9b60f1d7e991889c2c7e31d9c5688feaeee76e7d
itai257 <itai257@gmail.com>
2017-12-19 23:25:22 +0200

    firsttt

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { ProductListComponent } from "./products/product-list.component";
+import { convertToSpacesPipe } from "./shared/convert-to-spaces.pipe";
+
+@NgModule({
+  declarations: [
+      AppComponent,
+      ProductListComponent,
+      convertToSpacesPipe
+      
+  ],
+  imports: [
+      BrowserModule,
+      FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
